368534a65fbffec33778e89c2d0dcf8fe80920a8
rubini124 <rubinid@uw.edu>
2019-08-19 22:30:53 -0700

    Added lesson notes

---
+++ src/api/users.js
@@ -0,0 +1,15 @@
+const { NODE_ENV } = process.env;
+const BASE_URL = NODE_ENV === "development" ? "http://localhost:5000" : "tbd"; // Once we deploy, we need to change this
+
+export const getAllUsers = async () => {
+  const token = window.localStorage.getItem("journal_app");
+  const response = await fetch(`${BASE_URL}/api/users`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json"
+    },
+    method: "GET"
+  });
+  const json = await response.json();
+  return json.response;
+};
--- src/components/users/Container.js
+++ src/components/users/Container.js
@@ -1,37 +1,39 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
+import React from "react";
+import { Route } from "react-router-dom";
+import * as api from "../../api/users";
 
-import List from './List/List'
-import PostsContainer from '../posts/Container'
+import List from "./List/List";
+import PostsContainer from "../posts/Container";
 
 export default class Container extends React.Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      users: [
-        {
-          _id: '5de4',
-          username: 'example.user',
-          posts: [
-            {
-              _id: '6cj2',
-              content: 'This is an example post.',
-              emotion: 'joy',
-              created_at: new Date('2019-07-01')
-            }
-          ]
-        }
-      ]
+      users: [{ posts: [] }]
+    };
+  }
+
+  async componentDidMount() {
+    const token = window.localStorage.getItem("journal_app");
+    if (token) {
+      const users = await api.getAllUsers();
+      this.setState({ users });
     }
   }
 
-  render () {
-    const { users } = this.state
+  //removePost
+  // -- removes a single post from the state
+  //call set state
+
+  //refetchState
+
+  render() {
+    const { users } = this.state;
     return (
-      <main className='container'>
-        <Route path='/users' exact component={() => <List users={users} />} />
+      <main className="container">
+        <Route path="/users" exact component={() => <List users={users} />} />
         <PostsContainer users={users} />
       </main>
-    )
+    );
   }
 }
